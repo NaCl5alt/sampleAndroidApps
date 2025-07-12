b616517b98fd3726c219ef6c1d1a63f64dcbc8e0
5altNaCl <kaiouga0210@gmail.com>
2025-07-12 14:14:39 +0900

    CoilのImageLoaderの設定の追加

--- app/src/main/java/com/example/sampleandroidapps/SampleApplication.kt
+++ app/src/main/java/com/example/sampleandroidapps/SampleApplication.kt
@@ -1,7 +1,30 @@
 package com.example.sampleandroidapps
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
+import coil3.request.crossfade
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SampleApplication : Application()
+class SampleApplication : Application(), SingletonImageLoader.Factory {
+    override fun newImageLoader(context: PlatformContext): ImageLoader =
+        ImageLoader.Builder(context)
+            .crossfade(true)
+            .memoryCache {
+                MemoryCache.Builder()
+                    .maxSizePercent(context, 0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(context.cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .build()
+}
