b444b436a901810d2cbd0edb70565c9d956c8d7c
5altNaCl <kaiouga0210@gmail.com>
2025-07-31 16:50:00 +0900

    Timberを追加

--- app/src/main/java/com/example/sampleandroidapps/SampleApplication.kt
+++ app/src/main/java/com/example/sampleandroidapps/SampleApplication.kt
@@ -5,12 +5,22 @@ import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.SingletonImageLoader
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 import javax.inject.Inject
+
 
 @HiltAndroidApp
 class SampleApplication : Application(), SingletonImageLoader.Factory {
     @Inject
     lateinit var imageLoader: dagger.Lazy<ImageLoader>
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 
     override fun newImageLoader(context: PlatformContext): ImageLoader = imageLoader.get()
 }
