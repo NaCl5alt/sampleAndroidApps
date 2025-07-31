bbc9bc52e7c761aae405b5c60351611aeee551f2
5altNaCl <kaiouga0210@gmail.com>
2025-07-31 15:47:47 +0900

    アルバムのAppBarにSettingsアイコンボタンを追加

--- feature/coil/src/main/java/com/example/sampleandroidapps/feature/coil/screens/album/AlbumScreen.kt
+++ feature/coil/src/main/java/com/example/sampleandroidapps/feature/coil/screens/album/AlbumScreen.kt
@@ -6,8 +6,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -50,6 +54,15 @@ private fun AlbumScreen(
             TopAppBar(
                 title = {
                     Text(text = "Album")
+                },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            // Todo: Implementation of navigate to settings screen
+                        }
+                    ) {
+                        Icon(Icons.Default.Settings, contentDescription = "Settings")
+                    }
                 }
             )
         }
